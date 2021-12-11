ad8980d110bf5bae58b86f092eda6ef33edf9746
chanwooDev <pove2019@gmail.com>
2021-12-11 21:35:07 +0900

    [수정] firebase 테이블 변경 작업

--- lib/firebase_design/firebase_process.py
+++ lib/firebase_design/firebase_process.py
@@ -3,8 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import firestore
 import xml_parsing
-
-
+import conversing_address
 
 def makeDetailTable(db):
     detailDict = xml_parsing.getDetailDict(db)
@@ -23,34 +22,41 @@ def makeAreaToNameTable(db):
     #ciNames= []
     #ciSeqs = []
     areaDict = {}
+    gmaps = conversing_address.getGmaps()
     for doc in docs:
         tempDict = doc.to_dict()
         area = tempDict['area']
         if area not in areaDict:
-            areaDict[area] = {
-                'ciNames' : [],
-                'ciSeqs' : []
-            }
-        else:
-            ciName = tempDict['ciName']
-            ciSeq = tempDict['ciSeq']
-            areaDict[area]['ciNames'].append(ciName)
-            areaDict[area]['ciSeqs'].append(ciSeq)        
-    
-    for doc in areaDict.keys(): 
-        ref = db.collection('areaToName').document(doc)
-        ref.set(areaDict[doc])
-        print(doc, areaDict[doc])
+            areaDict[area] = []
+        else:     
+            if 'addr' in tempDict:
+                addr = tempDict['addr']
+                curDict = {}
+                curDict['ciName'] = tempDict['ciName']
+                curDict['category'] = tempDict['category']
+                lat, lng =  conversing_address.getGeocode(addr,gmaps)
+                if lat == 0:
+                    continue
+                curDict['lat'] = lat
+                curDict['lng'] = lng  
+                areaDict[area].append(curDict)
 
+    for doc in areaDict.keys(): 
+        ref = db.collection('areaToaNme').document(doc)
+        ref.set({'playground':areaDict[doc]})
 
 def getFirestoreClient():
-    cred = credentials.Certificate('lib/firebase_design/myKeys.json')
+    cred = credentials.Certificate('lib/firebase_design/firebaseKeys.json')
     firebase_admin.initialize_app(cred,
     {
         'databaseURL' : 'https://playmate-b7739.firebaseio.com'
     })
     return firestore.client()
 
-db = getFirestoreClient()
-makeDetailTable(db)
+
+
+#db = getFirestoreClient()
+#makeAreaToNameTable(db)
 #배열이안들어가네 
+
+
